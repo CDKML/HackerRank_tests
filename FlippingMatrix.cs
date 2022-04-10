5efbf2faaa540dbfbbeeaa91e28de3778109ca58
CDKML <kamalloco@hotmail.com>
2022-04-10 19:56:26 +0200

    - Add solution to flipping matrix problem.

---
+++ FlippingMatrix.cs
@@ -0,0 +1,71 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    /*
+     * Complete the 'flippingMatrix' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY matrix as parameter.
+     */
+
+    public static int flippingMatrix(List<List<int>> matrix)
+    {
+        int sum = 0;
+        int quadrantSize = matrix.Count() / 2;
+        for (int row = 0; row < quadrantSize; row++)
+        {
+            for (int column = 0; column < quadrantSize; column++)
+            {
+                int p1 = matrix[row][2 * quadrantSize - column - 1];
+                int p2 = matrix[row][column];
+                int p3 = matrix[2 * quadrantSize - row - 1][column];
+                int p4 = matrix[2 * quadrantSize - row - 1][2 * quadrantSize - column - 1];
+                sum += Math.Max(p1, Math.Max(p2, Math.Max(p3, p4)));
+            }
+        }
+        return sum;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<List<int>> matrix = new List<List<int>>();
+
+            for (int i = 0; i < 2 * n; i++)
+            {
+                matrix.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(matrixTemp => Convert.ToInt32(matrixTemp)).ToList());
+            }
+
+            int result = Result.flippingMatrix(matrix);
+
+            Console.WriteLine(result);
+            //textWriter.WriteLine(result);
+        }
+
+        //textWriter.Flush();
+        //textWriter.Close();
+    }
+}
